15f73e9cf1d45f98fdd76f57e30885713641f612
Sanjib Adhya <sanjib@zacksresearch.in>
2024-12-19 06:56:56 -0600

    learned scrubbing

--- src/js/main.js
+++ src/js/main.js
@@ -7,25 +7,15 @@ gsap.to('.square', {
 	duration: 8,
 	scrollTrigger: {
 		trigger: '.square',
-		start: 'top 60%',
-		end: 'top 40%',
-		//end: () => `+=${document.querySelector('.square').offsetHeight}`,
-		// markers: {
-		// 	startColor: 'purple',
-		// 	endColor: 'fuchsia',
-		// 	fontSize: '4rem',
-		// 	indent: 200,
-		// },
-		toggleActions: 'restart pause resume reverse',
-		//  onEnter onLeave onEnterBack onLeaveBack
-
-		//additional value: play, complete, reset, none
+		start: 'top 80%',
+		end: 'top 30%',
+		//scrub: true,
+		scrub: 4,
+		toggleActions: 'restart none none none',
 		markers: {
 			startColor: 'purple',
 			endColor: 'fuchsia',
 			fontSize: '3rem',
 		},
-		//markers: true,
-		toggleClass: 'red',
 	},
 });

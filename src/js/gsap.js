605eba74696b5ae2f62329af36b3ef7ee33e6b62
Sanjib Adhya <sanjib@zacksresearch.in>
2024-12-19 08:04:18 -0600

    learned how to use standalone ScrollTrigger

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -1,3 +1,5 @@
+import '../scss/styles.scss';
+
 //importing GSAP and ScrollTrigger
 import { gsap } from 'gsap';
 
@@ -6,3 +8,17 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 export * from 'gsap';
+
+//standalone scrollTrigger
+ScrollTrigger.create({
+	trigger: '.box',
+	start: 'top 80%',
+	end: 'top 50%',
+	markers: true,
+	toggleClass: 'box-red',
+	// onEnter: () => console.log('enter!'),
+	// onLeave: () => console.log('leave!'),
+	// onEnterBack: () => console.log('enterBack!'),
+	// onLeaveBack: () => console.log('leaveBack!'),
+	onUpdate: (self) => console.log(self),
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,3 @@
 //importing scss file
 import '../scss/styles.scss';
 import { gsap } from './gsap.js';
-
-//timeline
-// gsap.to('.box', { x: 500, duration: 2 });
-// gsap.to('.box', { y: 200, duration: 3, delay: 2 });
-// gsap.to('.box', { x: 0, duration: 2, delay: 5 });
-
-const tl = gsap.timeline({
-	scrollTrigger: {
-		trigger: '.box',
-		markers: true,
-		start: 'top 80%',
-		end: 'top 30%',
-		scrub: 1,
-	},
-});
-
-tl.to('.box', { x: 500, duration: 5 })
-	.to('.box', { y: 700, duration: 2 })
-	.to('.box', { x: 0, duration: 2 });
